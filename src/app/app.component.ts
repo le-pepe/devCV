17862c6066d3b25c8349c7651432bbb141ae88fd
José González <gonzalezmunozjosearmando@gmail.com>
2017-12-02 19:37:25 -0300

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare let jquery: any;
+declare let $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    /*======= Skillset *=======*/
+    $('.level-bar-inner').css('width', '0');
+    $('.level-bar-inner').each(function () {
+      const itemWidth = $(this).data('level');
+      $(this).animate({
+        width: itemWidth
+      }, 800);
+    });
+  }
+
 }
